b9a070850ce4c51f08c6b018e76f85064ffc1dac
Aleksandr Smirnov <genesboard@gmail.com>
2025-09-16 10:48:28 +0300

    eslint fixes

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,5 +1,5 @@
-import { InertiaLinkProps } from '@inertiajs/vue3';
-import type { LucideIcon } from 'lucide-vue-next';
+import { InertiaLinkProps } from '@inertiajs/vue3'
+import type { LucideIcon } from 'lucide-vue-next'
 
 export interface Auth {
     user: User;
