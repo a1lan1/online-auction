0b9a2bfec026ae5ff48d39c2275e8193e989fae7
Aleksandr Smirnov <genesboard@gmail.com>
2025-09-29 10:34:20 +0300

    add search reindex artisan cmd

---
+++ app/Console/Commands/SearchReindex.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Lot;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class SearchReindex extends Command
+{
+    /**
+     * @var string
+     */
+    protected $signature = 'search:reindex';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Flush and re-import all searchable models';
+
+    public function handle(): void
+    {
+        $models = [
+            Lot::class,
+        ];
+
+        foreach ($models as $model) {
+            $this->info("Flushing index for model: {$model}");
+            Artisan::call('scout:flush', ['model' => $model]);
+
+            $this->info("Importing data for model: {$model}");
+            Artisan::call('scout:import', ['model' => $model]);
+        }
+
+        $this->info('Search re-indexing completed successfully!');
+    }
+}
